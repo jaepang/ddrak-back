75ab731d4f369547b9f3dbc8fc6126a5a3d47f4a
Jaepang <jake_king@kakao.com>
2021-03-14 12:07:11 +0900

    :racehorse: improved auth

--- ddrakCalendar/serializers.py
+++ ddrakCalendar/serializers.py
@@ -7,7 +7,7 @@ from .models import Event
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('username', 'is_superuser', 'groups')
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
