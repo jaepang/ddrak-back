0348178d40e4fc274d6d19b5d0809fcf7dea3b16
jaepang <jake_king@kakao.com>
2022-01-13 00:37:59 +0900

    change db to postgresql

--- ddrakapi/config/settings/deploy.py
+++ ddrakapi/config/settings/deploy.py
@@ -4,7 +4,7 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': 'admin',
         'PASSWORD': os.environ.get("DB_PASSWORD")
